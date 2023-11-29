d8538da069e16f1dcd1db59d26a933e55cced1f9
Daniel Vazquez <fienbeh@gmail.com>
2023-11-29 15:35:24 -0600

    Agregar archivos de proyecto.

---
+++ Practica4.cpp
@@ -0,0 +1,34 @@
+// Practica4.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+	int myNum = 5;               // Integer (whole number without decimals)
+	double myFloatNum = 5.99;    // Floating point number (with decimals)
+	char myLetter = 'D';         // Character
+	std::string myText = "Hello";     // String (text)
+	bool myBoolean = true;       // Boolean (true or false)
+
+	std::cout << myNum;
+	std::cout << "\n";
+	std::cout << myLetter;	
+	std::cout << "\n";
+	std::cout << myFloatNum;
+	std::cout << "\n";
+	std::cout << myText;	
+	std::cout << "\n";
+	std::cout << myBoolean;
+	return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
